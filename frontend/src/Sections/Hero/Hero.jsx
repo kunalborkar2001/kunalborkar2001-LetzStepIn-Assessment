f43d9a155b874048071b914bfc52fff094de097d
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-07 14:41:14 +0530

    Added Navigator

--- frontend/src/Sections/Hero/Hero.jsx
+++ frontend/src/Sections/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="Hero">
             <div className="heroLeft">
-                <h1>Virtual Reality based <br /> Search Portal for <br /> Real Existing Spaces</h1>
+                <h1>Virtual Reality <span style={{color : "red"}}>based <br /> Search Portal</span>  for <br /> Real Existing Spaces</h1>
                 <p>A digital platform that allows users to <br /> search, explore, and experience real-world environments through virtual reality technology.</p>
             </div>
             <div className="heroRight">
--- frontend/src/Sections/Navigator/Navigator.jsx
+++ frontend/src/Sections/Navigator/Navigator.jsx
@@ -1,39 +1,97 @@
 import "./Navigator.css"
 import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const Navigator = () => {
     return (
         <div className="Navigator">
             <ul>
                 <li>
-                    <Button variant="outlined">
-                        Education
-                    </Button>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} >
+                        <InputLabel id="Education">Education</InputLabel>
+                        <Select
+                            labelId="Education"
+                            id="Education"
+                            label="Education"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </li>
                 <li>
-                    <Button variant="outlined">
-                        Hotel/Room Booking
-                    </Button>
+                    <FormControl sx={{ m: 1, minWidth: 200 }} >
+                        <InputLabel id="Hotel/Room">Hotel/Room Booking</InputLabel>
+                        <Select
+                            labelId="Hotel/Room"
+                            id="Hotel/Room"
+                            label="Hotel/Room"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </li>
                 <li>
-                    <Button variant="outlined">
-                        Property/Real-Estate
-                    </Button>
+                    <FormControl sx={{ m: 1, minWidth: 200 }} >
+                        <InputLabel id="Property/Real-Estate">Property/Real-Estate</InputLabel>
+                        <Select
+                            labelId="Property/Real-Estate"
+                            id="Property/Real-Estate"
+                            label="Property/Real-Estate"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </li>
                 <li>
-                    <Button variant="outlined">
-                        Restaurants & Cafes
-                    </Button>
+                    <FormControl sx={{ m: 1, minWidth: 200 }} >
+                        <InputLabel id="Restaurants">Restaurants & Cafes</InputLabel>
+                        <Select
+                            labelId="Restaurants"
+                            id="Restaurants"
+                            label="Restaurants"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </li>
                 <li>
-                    <Button variant="outlined">
-                        Pubs
-                    </Button>
+                    <FormControl sx={{ m: 1, minWidth: 80 }} >
+                        <InputLabel id="Pubs">Pubs</InputLabel>
+                        <Select
+                            labelId="Pubs"
+                            id="Pubs"
+                            label="Pubs"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </li>
                 <li>
-                    <Button variant="outlined">
-                        Banquet Halls
-                    </Button>
+                    <FormControl sx={{ m: 1, minWidth: 150 }} >
+                        <InputLabel id="Banquet">Banquet Halls</InputLabel>
+                        <Select
+                            labelId="Banquet"
+                            id="Banquet"
+                            label="Banquet"
+                        >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </li>
             </ul>
         </div>
