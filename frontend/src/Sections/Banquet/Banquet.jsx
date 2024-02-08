8deae3be5586ce00ee1988e7945e751c9609cf74
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-08 12:39:53 +0530

    Added Banquet cards

--- frontend/src/Sections/Banquet/Banquet.jsx
+++ frontend/src/Sections/Banquet/Banquet.jsx
@@ -1,11 +1,28 @@
+import SlidingCard from "../../Components/SlidingCard/SlidingCard";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import banquet1 from "../../Assets/Banquet/banquet1.jpeg"
 import "./Banquet.css"
 
 const Banquet = () => {
   return (
     <div className="Banquet">
-      <h1>
-        Banquet
-      </h1>
+      <div >
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <h1>Banquet</h1>
+          </AccordionSummary>
+          <AccordionDetails className='accordionCards'>
+            <SlidingCard image={banquet1}/>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </div>
   )
 }
