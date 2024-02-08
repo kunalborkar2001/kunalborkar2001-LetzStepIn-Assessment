8495d45cffe22a0780bbdef5ce5fc4dfed5c5084
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-08 13:10:55 +0530

    Added Hotel and Property cards

--- frontend/src/Sections/Hotels/Hotels.jsx
+++ frontend/src/Sections/Hotels/Hotels.jsx
@@ -1,11 +1,30 @@
+import SlidingCard from "../../Components/SlidingCard/SlidingCard";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import hotel1 from "../../Assets/Hotel/hotel1.jpeg"
+import hotel2 from "../../Assets/Hotel/hotel2.jpeg"
 import "./Hotels.css"
 
 const Hotels = () => {
   return (
     <div className="Hotels">
-      <h1>
-        Hotels
-      </h1>
+      <div >
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <h1>Hotels</h1>
+          </AccordionSummary>
+          <AccordionDetails className='accordionCards'>
+            <SlidingCard image={hotel1} />
+            <SlidingCard image={hotel2} />
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </div>
   )
 }
--- frontend/src/Sections/Property/Property.jsx
+++ frontend/src/Sections/Property/Property.jsx
@@ -1,11 +1,28 @@
+import SlidingCard from "../../Components/SlidingCard/SlidingCard";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import property1 from "../../Assets/Property/property1.jpeg"
 import "./Property.css"
 
 const Property = () => {
   return (
     <div className="Property">
-      <h1>
-        Property
-      </h1>
+      <div >
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <h1>Property</h1>
+          </AccordionSummary>
+          <AccordionDetails className='accordionCards'>
+            <SlidingCard image={property1} />
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </div>
   )
 }
