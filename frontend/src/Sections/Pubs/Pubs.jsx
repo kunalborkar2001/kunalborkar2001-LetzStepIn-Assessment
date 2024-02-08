644aeb746af5aafbfdcbfb5303e226ad15736963
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-02-08 13:27:25 +0530

    Added Restaurants card

--- frontend/src/Sections/Pubs/Pubs.jsx
+++ frontend/src/Sections/Pubs/Pubs.jsx
@@ -29,8 +29,6 @@ const Pubs = () => {
           </AccordionDetails>
         </Accordion>
 
-
-
       </div>
     </div>
   );
--- frontend/src/Sections/Restaurants/Restaurants.jsx
+++ frontend/src/Sections/Restaurants/Restaurants.jsx
@@ -1,11 +1,34 @@
+import React from 'react';
+import SlidingCard from "../../Components/SlidingCard/SlidingCard";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import restaurant1 from '../../Assets/Restaurant/restaurant1.jpeg'
+import restaurant2 from '../../Assets/Restaurant/restaurant2.jpeg'
+import restaurant3 from '../../Assets/Restaurant/restaurant3.jpeg'
 import "./Restaurants.css"
 
 const Restaurants = () => {
   return (
     <div className="Restaurants">
-      <h1>
-        Restaurants
-      </h1>
+      <div >
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+            <h1>Restaurants</h1>
+          </AccordionSummary>
+          <AccordionDetails className='accordionCards'>
+            <SlidingCard image={restaurant1}/>
+            <SlidingCard image={restaurant2}/>
+            <SlidingCard image={restaurant3}/>
+          </AccordionDetails>
+        </Accordion>
+
+      </div>
     </div>
   )
 }
